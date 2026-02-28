900025b66067bad830d48c1a8b1addff53823415
Nishu <nishu.ptg@gmail.com>
2026-02-28 20:30:18 +0600

    02: routing & controllers (with simple json in index for now) + added missing unique constraints in categories table

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('categories', CategoryController::class);
+Route::resource('authors', AuthorController::class);
+Route::resource('books', BookController::class);
